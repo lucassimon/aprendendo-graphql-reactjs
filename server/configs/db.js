e9cbd06b6ea65054b2a47bf36144109912ee219e
Lucas Simon <lucassrod@gmail.com>
2017-07-04 00:09:13 -0300

    Cria configurações com o dottenv

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
+
 let Mongoose = require('mongoose')
 
-const db = Mongoose.connect('mongodb://localhost/sibelius').connection
+const db = Mongoose.connect(process.env.DATABASE_URL).connection
 
 export default db;
