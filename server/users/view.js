01af00ce0b43d10aa7f55ea4105a4c1f67f776e1
Lucas Simon <lucassrod@gmail.com>
2017-07-05 16:41:12 -0300

    Faz a busca por todos os usuarios do sistema

--- server/users/view.js
+++ server/users/view.js
@@ -1,11 +1,31 @@
-'use strict'
 
 let User = require('./model')
 
 exports.find = async (ctx, next) => {
+
+  // Não entendi uma coisa no meu db.js quando coloco promiseLibrary para
+  // bluebird não consigo realizar a pesquisa no mongo gerando timeout.
+  // por conta disso estou utilizando o padrão do mongoose
+
+  // todo: gerar ctx de resultados dentro retorno com sucesso da pesquisa
+
+  // todo: gerar retorno do contexto dentro do bloco catch retornando a api
+  // com erro
+
+  let users =  await User.find({}).select({password:0}).exec().then(
+    (results) => {
+      return results
+    }
+  ).catch(
+    (error) => {
+      console.log(error)
+    }
+  )
+
   ctx.body = {
     'resource': 'Usuários',
     'message': 'Lista todos os usuários',
+    'data': users
   }
 }
 
