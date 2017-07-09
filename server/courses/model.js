276922abff3530e5048396ef3de2aa42dec89807
Lucas Simon <lucassrod@gmail.com>
2017-07-08 22:53:30 -0300

    Cria o modelo de cursos

--- server/courses/model.js
+++ server/courses/model.js
@@ -1,7 +1,32 @@
 let Mongoose = require('mongoose')
 
-const Course = new Mongoose.Schema({
-  name: String
+const states = ['required', 'optional', 'additional']
+const Discipline = new Mongoose.Schema({
+  order: { type: Number, min: 1 },
+  name: { type: String, required: true },
+  ch: { type: Number, max: 240, min: 40 },
+  cr: { type: Number, max: 8, min: 0 },
+  kind: {  String, enum: states }
 })
 
-export default Mongoose.model('Course', Course);
+const Curriculum = new Mongoose.Schema({
+  order: { type: Number, min: 1 },
+  name: { type: String, required: true},
+  disciplines: [Discipline]
+})
+
+const Course = new Mongoose.Schema({
+  university: { type: String, required: true, trim: true},
+  campus: { type: String, required: true, trim: true},
+  name: { type: String, required: true, trim: true },
+  category: { type: String, required: true, trim: true},
+  description: {type: String, required: false},
+  goals: {type: String, required: false},
+  duration: { type: String, required: true},
+  modality: { type: String, required: true},
+  grid: [Curriculum],
+  created: { type: Date },
+  updated: { type: Date, default: Date.now }
+})
+
+module.exports = Mongoose.model('Course', Course);
