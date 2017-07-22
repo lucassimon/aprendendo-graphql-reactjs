c5020b7589991be8c6666637ec6081009c094cd0
Lucas Simon <lucassrod@gmail.com>
2017-07-22 16:18:07 -0300

    Cria um novo tipo para fazer payload dos dados

--- server/courses/graphql/types.js
+++ server/courses/graphql/types.js
@@ -26,19 +26,60 @@ let courseType = new graphql.GraphQLObjectType({
     },
     category: {
       type: new graphql.GraphQLNonNull(graphql.GraphQLString),
-      description: 'Category represents the aggregate of many courses. Eg. Engineering'
+      description: 'Category represents the aggregate of many courses. Eg. Engineering and Technologies'
     },
     duration: {
-      type: new graphql.GraphQLNonNull(graphql.GraphQLString)
+      type: new graphql.GraphQLNonNull(graphql.GraphQLString),
       description: 'The total period in this course'
     },
     modality: {
-      type: new graphql.GraphQLNonNull(graphql.GraphQLString)
+      type: new graphql.GraphQLNonNull(graphql.GraphQLString),
       description: 'The modality. Eg. Master degree'
+    },
+    description: {
+      type: new graphql.GraphQLNonNull(graphql.GraphQLString),
+      description: 'Information of this course'
+    },
+    goals: {
+      type: new graphql.GraphQLNonNull(graphql.GraphQLString),
+      description: 'The goals of this course'
     }
     // grid: { type: new graphql.GraphQLList(gridType) }
   }
 })
 
+let courseInputType = new graphql.GraphQLInputObjectType({
+  name: 'CourseInput',
+  description: 'Course mutation',
+  fields: () => ({
+    university: {
+      type: new graphql.GraphQLNonNull(graphql.GraphQLString),
+      description: 'The university that offers this course'
+    },
+    campus: {
+      type: new graphql.GraphQLNonNull(graphql.GraphQLString),
+      description: 'Campus or local of that this course is teach'
+    },
+    name: {
+      type: new graphql.GraphQLNonNull(graphql.GraphQLString),
+      description: 'The name of course'
+    },
+    category: {
+      type: new graphql.GraphQLNonNull(graphql.GraphQLString),
+      description: 'Category represents the aggregate of many courses. Eg. Engineering and Technologies'
+    },
+    duration: {
+      type: new graphql.GraphQLNonNull(graphql.GraphQLString),
+      description: 'The total period in this course'
+    },
+    modality: {
+      type: new graphql.GraphQLNonNull(graphql.GraphQLString),
+      description: 'The modality. Eg. Master degree'
+    },
+  })
+})
 
-module.exports = courseType
+module.exports = {
+  CourseType: courseType,
+  CourseInputType: courseInputType
+}
