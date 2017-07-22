90002cc6bf536e6f147cf45f064ad677dd8ab169
Lucas Simon <lucassrod@gmail.com>
2017-07-22 15:49:31 -0300

    Cria descriçoes para os campos do curso

--- server/courses/graphql/types.js
+++ server/courses/graphql/types.js
@@ -8,13 +8,35 @@ let courseType = new graphql.GraphQLObjectType({
   name:'Course',
   description: 'Course Model',
   fields: {
-    id: { type: new graphql.GraphQLNonNull(graphql.GraphQLID) },
-    name: { type: new graphql.GraphQLNonNull(graphql.GraphQLString) },
-    university: { type: new graphql.GraphQLNonNull(graphql.GraphQLString) },
-    campus: { type: new graphql.GraphQLNonNull(graphql.GraphQLString) },
-    category: { type: new graphql.GraphQLNonNull(graphql.GraphQLString) },
-    duration: { type: new graphql.GraphQLNonNull(graphql.GraphQLString) },
-    modality: { type: new graphql.GraphQLNonNull(graphql.GraphQLString) }
+    id: {
+      type: new graphql.GraphQLNonNull(graphql.GraphQLID),
+      description: 'Refer a objectId MongoDB'
+    },
+    name: {
+      type: new graphql.GraphQLNonNull(graphql.GraphQLString),
+      description: 'The name of course'
+    },
+    university: {
+      type: new graphql.GraphQLNonNull(graphql.GraphQLString),
+      description: 'The university that offers this course'
+    },
+    campus: {
+      type: new graphql.GraphQLNonNull(graphql.GraphQLString),
+      description: 'Campus or local of that this course is teach'
+    },
+    category: {
+      type: new graphql.GraphQLNonNull(graphql.GraphQLString),
+      description: 'Category represents the aggregate of many courses. Eg. Engineering'
+    },
+    duration: {
+      type: new graphql.GraphQLNonNull(graphql.GraphQLString)
+      description: 'The total period in this course'
+    },
+    modality: {
+      type: new graphql.GraphQLNonNull(graphql.GraphQLString)
+      description: 'The modality. Eg. Master degree'
+    }
+    // grid: { type: new graphql.GraphQLList(gridType) }
   }
 })
 
