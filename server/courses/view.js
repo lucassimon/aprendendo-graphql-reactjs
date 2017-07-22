63b784ad31bc9e1ad76290c9f1b46549f8ca66d7
Lucas Simon <lucassrod@gmail.com>
2017-07-22 14:23:43 -0300

    Adiciona o metodo para adicionar o curso

--- server/courses/view.js
+++ server/courses/view.js
@@ -1,7 +1,25 @@
 'use strict'
 
+let mongoose = require('mongoose')
+let Course = require('./model')
+
+
 exports.find = async (ctx, next) => {
     ctx.body = {
       resource: 'Cursos'
     }
+}
+
+// View para adicionar um curso
+exports.add = async (ctx, next) => {
+
+  let course = new Course(ctx.request.body)
+
+  await course.save()
+
+  ctx.body = {
+    'resource': 'Cursos',
+    'message': 'Adicionar curso',
+    'data': course
+  }
 }
