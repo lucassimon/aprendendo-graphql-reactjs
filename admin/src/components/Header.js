34d8be848ff5dbe1e82787fce8d3561471314723
Lucas Simon <lucassrod@gmail.com>
2017-09-26 18:58:34 -0300

    Cria o Header com o component NavigationTop

--- admin/src/components/Header.js
+++ admin/src/components/Header.js
@@ -1,24 +1,12 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom'
+import NavigationTop from './NavigationTop'
 
 const Header = () => {
 
   return (
-    <header>
-      <nav>
-        <ul>
-          <li>
-            <Link to='/'> Dashboard </Link>
-          </li>
-          <li>
-            <Link to='/users'> Usuários </Link>
-          </li>
-          <li>
-            <Link to='/courses'> Cursos </Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <div>
+      <NavigationTop />
+    </div>
   )
 }
 
