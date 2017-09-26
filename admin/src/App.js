a065e01ebd87eb56617552cc3c63ba6fc3418fa7
Lucas Simon <lucassrod@gmail.com>
2017-09-26 18:58:16 -0300

    Adiciona o bulma.css

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './statics/css/bulma.css';
 import Header from './components/Header'
 import Main from './containers/Main'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
+      <div>
+        <div>
           <Header />
         </div>
         <div>
