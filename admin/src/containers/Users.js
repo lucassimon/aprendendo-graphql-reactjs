af05637a107eca325047be1b6bae7e4b15fc4509
Lucas Simon <lucassrod@gmail.com>
2017-09-26 20:47:26 -0300

    Cria um state com dados fake de usuarios
    Renderiza o componente list

--- admin/src/containers/Users.js
+++ admin/src/containers/Users.js
@@ -1,16 +1,64 @@
 import React, { Component } from 'react'
 import Hero from '../components/Hero'
+import UserList from './UsersList'
+
 
 class Users extends Component {
 
   constructor(props) {
     super(props)
+
+    this.state = {
+      users: [
+        {
+          id: 1,
+          name:'User 1',
+          email:'user1@test.com',
+          active: true,
+          is_admin: true,
+          is_superuser: true
+        },
+        {
+          id: 2,
+          name:'User 2',
+          email:'user2@test.com',
+          active: false,
+          is_admin: false,
+          is_superuser: false
+        },
+        {
+          id: 3,
+          name:'User 3',
+          email:'user3@test.com',
+          active: false,
+          is_admin: false,
+          is_superuser: false
+        },
+        {
+          id: 4,
+          name:'User 4',
+          email:'user4@test.com',
+          active: false,
+          is_admin: false,
+          is_superuser: false
+        },
+        {
+          id: 5,
+          name:'User 5',
+          email:'user5@test.com',
+          active: true,
+          is_admin: false,
+          is_superuser:false
+        },
+      ]
+    }
   }
 
   render() {
     return (
       <div>
-        <Hero title="Usuários" subtitle="Nossos usuários" />
+        <Hero title="Usuários" subtitle="Listagem de usuários" />
+        <UserList items={this.state.users} />
       </div>
     )
   }
