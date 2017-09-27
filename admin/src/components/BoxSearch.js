ec471ac9f416676801b83c4f0dc94cb576b0bcac
Lucas Simon <lucassrod@gmail.com>
2017-09-27 19:46:20 -0300

    Cria funções para manipular os inputs e selectbox

--- admin/src/components/BoxSearch.js
+++ admin/src/components/BoxSearch.js
@@ -1,40 +1,109 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 
-const BoxSearch = (props) => {
-  const fields = props.fields.map((item, index)=>{
+class BoxSearch extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      query: '',
+      field: this.props.fields[0].key,
+      operator: this.props.operators[0].key
+    }
+
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  componentDidMount() {
+    console.log(this.state)
+  }
+
+  handleChange(event) {
+    this.setState({[event.target.name]: event.target.value});
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  render() {
+
+
+    const fields = this.props.fields.map((item, index)=>{
+      return (
+        <option
+          key={index}
+          value={item.key}
+        >
+          {item.value}
+        </option>
+      )
+    })
+    const operators = this.props.operators.map((item, index)=>{
+      return (
+        <option
+          key={index}
+          value={item.key}
+        >
+          {item.value}
+        </option>
+      )
+    })
     return (
-      <option
-        key={index}
-        value={item.key}
-      >
-        {item.value}
-      </option>
-    )
-  })
-  return (
-    <div className="columns">
-      <div className="column is-12">
-        <div className="field has-addons">
-          <p className="control">
-            <span className="select">
-              <select>
-                {fields}
-              </select>
-            </span>
-          </p>
-          <p className="control is-expanded">
-            <input className="input" type="text" placeholder="Informe um texto para pesquisa" />
-          </p>
-          <p className="control">
-            <a className="button is-info">
-              Pesquisar
-            </a>
-          </p>
+      <div className="columns">
+        <div className="column is-12">
+          <div className="field has-addons">
+            <p className="control">
+              <span className="select">
+                <select
+                  name='field'
+                  value={this.state.field}
+                  onChange={this.handleChange}
+                >
+                  {fields}
+                </select>
+                <span>{this.state.field}</span>
+              </span>
+            </p>
+            <p className="control">
+              <span className="select">
+                <select
+                  name='operator'
+                  value={this.state.operator}
+                  onChange={this.handleChange}
+                >
+                  {operators}
+                </select>
+                <span>{this.state.operator}</span>
+              </span>
+            </p>
+            <p className="control is-expanded">
+              <input
+                className="input"
+                name='query'
+                type="text"
+                value={this.state.query}
+                onChange={this.handleInputChange}
+                placeholder="Informe um texto para pesquisa"
+              />
+            </p>
+            <p className="control">
+              <a className="button is-info">
+                Pesquisar
+              </a>
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default BoxSearch
