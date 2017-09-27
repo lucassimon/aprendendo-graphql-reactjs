cfaa0bb46569ed76ef0bb2c821f754b9d30794f2
Lucas Simon <lucassrod@gmail.com>
2017-09-27 11:33:05 -0300

    Cria componente para lancar modal de criacao de usuario

---
+++ admin/src/components/users/BtnAddUser.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const BtnAddUser = (props) => {
+
+    return (
+        <div className="columns is-mobile">
+            <div className="column is-4 is-offset-8 has-text-right">
+                <a className="button is-primary">
+                    Adicionar
+                </a>
+            </div>
+        </div>
+    )
+}
+
+export default BtnAddUser
