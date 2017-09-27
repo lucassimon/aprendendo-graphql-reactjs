706537d0659393ee775e262142cb16abe1e50ba0
Lucas Simon <lucassrod@gmail.com>
2017-09-27 11:32:52 -0300

    Cria compoenente para filtrar usuarios

---
+++ admin/src/components/users/BoxFilterUser.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+
+const BoxFilterUser = (props) => {
+
+  return (
+    <nav className="panel">
+      <p className="panel-heading">
+        Filtros
+      </p>
+      <label className="panel-block">
+        <input type="checkbox" />
+        Está Ativo?
+      </label>
+      <label className="panel-block">
+        <input type="checkbox" />
+        É admin?
+      </label>
+      <label className="panel-block">
+        <input type="checkbox" />
+        É super usuário?
+      </label>
+      <div className="panel-block">
+        <button className="button is-primary is-outlined is-fullwidth">
+          Limpar Filtros
+        </button>
+      </div>
+    </nav>
+  )
+}
+
+export default BoxFilterUser
