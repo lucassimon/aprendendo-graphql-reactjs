0a5e1fb8ba4f88da392b90698c0cb36caffed581
Lucas Simon <lucassrod@gmail.com>
2017-09-28 15:53:13 -0300

    Cria a lógica de busca de usuario neste componente com seus estados

--- admin/src/containers/users/UsersList.js
+++ admin/src/containers/users/UsersList.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
+
+// Services
+
+import UserService from '../../services/UserService'
+
+// Components
+
 import BoxUser from '../../components/users/BoxUser'
-import BtnAddUser from '../../components/users/BtnAddUser'
 import BoxSearch from '../../components/BoxSearch'
 import BoxSearchPerPage from '../../components/BoxSearchPerPage'
 import BoxFilterUser from '../../components/users/BoxFilterUser'
@@ -13,6 +19,18 @@ class UserList extends Component {
 
     this.state = {
       name: 'Usuário(s)',
+      search_args: {
+        field: null,
+        query: null,
+        operator: null,
+        per_page: 10,
+        is_active: null,
+        is_admin: null,
+        is_superuser: null,
+        page: 1
+      },
+      users: [],
+      count_users: 0,
       fields:[
         {key:'email', value:'Email'},
         {key:'name', value:'Name'}
@@ -23,11 +41,56 @@ class UserList extends Component {
         {key:'like', value:'Contém'}
       ]
     }
+
+    this.updateSearchParams = this.updateSearchParams.bind(this)
+    this.search = this.search.bind(this)
+  }
+
+  updateSearchParams(args) {
+
+    // dica
+    // const search_args = {
+    //     ...(this.state.searchArgs || {}),
+    //     ...(args || {})
+    // };
+
+    let { search_args } = this.state;
+    search_args.field = args.field
+    search_args.operator = args.operator
+    search_args.query = args.query
+
+    this.setState({ search_args }, () => this.search())
+
+  }
+
+  search() {
+    console.log(this.state.search_args)
+    // TODO: Atraves do estado atual realizar a chamada no servidor
+  }
+
+  componentDidMount() {
+    UserService.getUsersPerPage(this.state.search_args)
+    .then(
+      (data) => {
+
+        if (data.success) {
+          this.setState({
+            users: data.data,
+            count_users: data.total
+          })
+        }
+
+      }
+    ).catch(
+      (error) => {
+        console.log('tratar status code response', error)
+      }
+    )
   }
 
   render() {
 
-    const items = this.props.items.map((item, index)=>{
+    const items = this.state.users.map((item, index)=>{
       return (
         <BoxUser
           key={index}
@@ -45,15 +108,14 @@ class UserList extends Component {
       <section className="">
         <div className="container">
 
-          <BtnAddUser />
-
           <BoxSearch
             fields={this.state.fields}
             operators={this.state.operators}
+            updateSearchParams={this.updateSearchParams}
           />
 
           <BoxSearchPerPage
-            count={this.props.total}
+            count={this.state.count_users}
             name={this.state.name}
           />
 
