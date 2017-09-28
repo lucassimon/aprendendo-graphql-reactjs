06162cd025890538eefa893ff94df008590b6aff
Lucas Simon <lucassrod@gmail.com>
2017-09-28 15:52:45 -0300

    Remove os stats de usuarios do container principal

--- admin/src/containers/Users.js
+++ admin/src/containers/Users.js
@@ -1,53 +1,27 @@
 import React, { Component } from 'react'
 import Hero from '../components/Hero'
 import UserList from './users/UsersList'
-import UserService from '../services/UserService'
 
+import BtnAddUser from '../components/users/BtnAddUser'
 
 class Users extends Component {
 
   constructor(props) {
     super(props)
-
-    this.state = {
-      search_args: {
-        email: null,
-        per_page: 10,
-        is_active: null,
-        is_admin: null,
-        is_superuser: null,
-        page: 1
-      },
-      users: [],
-      count_users: 0
-    }
-  }
-
-  componentDidMount() {
-    UserService.getUsersPerPage(this.state.search_args)
-    .then(
-      (data) => {
-
-        if (data.success) {
-          this.setState({
-            users: data.data,
-            count_users: data.total
-          })
-        }
-
-      }
-    ).catch(
-      (error) => {
-        console.log('tratar status code response', error)
-      }
-    )
   }
 
   render() {
     return (
       <div>
         <Hero title="Usuários" subtitle="Listagem de usuários" />
-        <UserList items={this.state.users} total={this.state.count_users} />
+
+        <section className="">
+          <div className="container">
+            <BtnAddUser />
+          </div>
+        </section>
+
+        <UserList />
       </div>
     )
   }
