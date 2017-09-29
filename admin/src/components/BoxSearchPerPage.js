88f1c058d22d233f10605aaccc75239c4944fae9
Lucas Simon <lucassrod@gmail.com>
2017-09-29 16:16:15 -0300

    Transforma o componente em functional component

--- admin/src/components/BoxSearchPerPage.js
+++ admin/src/components/BoxSearchPerPage.js
@@ -4,33 +4,21 @@ import React from 'react';
 const BoxSearchPerPage = (props) => {
 
   return (
-    <div className="columns">
-      <div className="column is-two-thirds">
-        <h4 className='title is-4'>
-          Listando {props.count} {props.name}
-        </h4>
-      </div>
-      <div className="column">
-        <div className="field has-addons">
-          <div className="control is-expanded">
-            <div className="select is-fullwidth">
-              <select name="perPage">
-
-                <option value='10'>10</option>
-                <option value='20'>20</option>
-                <option value='30'>30</option>
-                <option value='40'>40</option>
-              </select>
-            </div>
-          </div>
-          <div className="control">
-            <button type="submit" className="button is-info">
-              Por Página
-            </button>
+    <div className="column">
+      <div className="field">
+        <div className="control is-expanded">
+          <div className="select is-fullwidth">
+            <select name="perPage" onChange={(event)=>props.updatePerPage(event.target.value)}>
+              <option value='10'>10 por página</option>
+              <option value='20'>20</option>
+              <option value='30'>30</option>
+              <option value='40'>40</option>
+            </select>
           </div>
         </div>
       </div>
     </div>
+
   )
 }
 
