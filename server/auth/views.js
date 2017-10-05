400eca54904cb640e1daf5bba3f155cc2d504bba
Lucas Simon <lucassrod@gmail.com>
2017-10-04 22:37:50 -0300

    Cria logica para gerar o token

--- server/auth/views.js
+++ server/auth/views.js
@@ -1,9 +1,44 @@
 'use strict'
 
+const jwt = require('./jwt')
+const User = require('../users/model')
+const jsonwebtoken = require("jsonwebtoken")
+
+
 exports.signin = async (ctx, next) => {
-  ctx.body = {
-    'resource': 'Auth',
-    'message': 'Login to API'
+  let email = ctx.request.body.email
+  let password = ctx.request.body.password
+
+  const user = await User.findOne({email:email}).then()
+
+  if (!user) {
+
+    ctx.status = 401;
+    ctx.body = {error: "Não encontramos este usuário"}
+
+  } else {
+
+    const match = await jwt.comparePassword(password, user)
+
+    if (match.bool) {
+      ctx.body = {
+        'resource': 'Auth',
+        'message': 'Login to API',
+        'token': jsonwebtoken.sign(
+          {
+            'id': user.id,
+            'email': user.get('email'),
+          },
+          jwt.secret,
+          { algorithm: 'HS256', expiresIn: '1h' }
+        ),
+        'is_admin': user.get('is_admin'),
+        'is_active': user.get('active')
+      }
+    } else {
+      ctx.status = 401;
+      ctx.body = {error: "A senha informada não confere"}
+    }
   }
 }
 
