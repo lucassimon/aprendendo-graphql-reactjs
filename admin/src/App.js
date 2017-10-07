297a67d5086c03a2244717d4acbcb69aa415dc1a
Lucas Simon <lucassrod@gmail.com>
2017-10-07 18:11:31 -0300

    Altera as rotas do app principal

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,22 +1,24 @@
 import React, { Component } from 'react';
-
+import { Switch, Route} from 'react-router-dom'
 import './statics/css/bulma.css';
-import Header from './components/Header'
+
+import Index from './containers/Index'
 import Main from './containers/Main'
+import Login from './containers/Login'
+import Register from './containers/Register'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <div>
-          <Header />
-        </div>
-        <div>
-          <Main />
-        </div>
-      </div>
+      <Switch>
+        <Route exact path="/" component={Index} /> {/* Note-1 */}
+        <Route exact path='/login' component={Login} />
+        <Route path='/register' component={Register} />
+        <Route path='/dashboard' component={Main} />
+      </Switch>
     );
   }
 }
 
 export default App;
+
