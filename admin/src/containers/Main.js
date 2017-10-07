649f9fdabd14b09ed1dbd4aadeba60a2573617c2
Lucas Simon <lucassrod@gmail.com>
2017-10-07 18:19:44 -0300

    Corrige as rotas e coloca NavigationTop

--- admin/src/containers/Main.js
+++ admin/src/containers/Main.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import { Switch, Route} from 'react-router-dom'
+import NavigationTop from '../components/NavigationTop'
 import Dashboard from './Dashboard'
 import Users from './Users'
 import Products from './Products'
 
-const Main = () => (
-  <main>
+const Main = ({match}) => (
+  <div>
+    <NavigationTop />
     <Switch>
-      <Route exact path='/' component={Dashboard}/>
-      <Route path='/users/' component={Users} />
-      <Route path='/products/' component={Products}/>
+      <Route exact path={`${match.url}/`} component={Dashboard}/>
+      <Route path={`${match.url}/users/`} component={Users} />
+      <Route path={`${match.url}/products/`} component={Products}/>
     </Switch>
-  </main>
+  </div>
 )
 
 export default Main
