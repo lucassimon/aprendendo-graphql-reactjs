08ae56dfc091b04d14532dfa3dae900b60743f57
Lucas Simon <lucassrod@gmail.com>
2017-10-16 19:03:40 -0200

    Corrige o navbar para a versao 6 do bulma

--- admin/src/components/NavigationTop.js
+++ admin/src/components/NavigationTop.js
@@ -4,51 +4,110 @@ import {NavLink} from 'react-router-dom'
 
 const NavigationTop = () => {
   return (
-    <nav className="nav has-shadow" aria-label="main navigation">
-      <div className="container">
-
-        <div className="nav-left">
-
-          <NavLink
+    <nav className="navbar is-dark">
+      <div className="navbar-brand">
+        <NavLink
             to='/'
             exact
-            className="nav-item">
-              <img src="https://dansup.github.io/bulma-templates/images/bulma.png" alt="Description" />
-          </NavLink>
+            className="navbar-item">
+          <img src="https://bulma.io/images/bulma-logo-white.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28" />
+        </NavLink>
+        <div className="navbar-burger burger" data-target="navMenuColorsuccess-example">
+          <span></span>
+          <span></span>
+          <span></span>
         </div>
-        <span className="nav-toggle">
-          <span></span>
-          <span></span>
-          <span></span>
-        </span>
-        <div className="nav-right nav-menu">
+      </div>
+      <div id="navMenuColorsuccess-example" className="navbar-menu">
+        <div className="navbar-start">
           <NavLink
             to='/dashboard/'
             exact
             strict
-            className="nav-item is-tab"
+            className="navbar-item"
             activeClassName="is-active"
           >
             Dashboard
           </NavLink>
-          <NavLink
-            to='/dashboard/users/'
-            exact
-            strict
-            className="nav-item is-tab"
-            activeClassName="is-active"
-          >
-            Users
-          </NavLink>
-          <NavLink
-            to='/dashboard/products/'
-            exact
-            strict
-            className="nav-item is-tab"
-            activeClassName="is-active"
-          >
-            Products
-          </NavLink>
+          <div className="navbar-item has-dropdown is-hoverable">
+            <NavLink
+              to='/dashboard/users/'
+              exact
+              strict
+              className="navbar-link"
+              activeClassName="is-active"
+            >
+              Users
+            </NavLink>
+            <div className="navbar-dropdown">
+              <NavLink
+                to='/dashboard/users/'
+                exact
+                strict
+                className="navbar-item"
+                activeClassName="is-active"
+              >               
+                Listar
+              </NavLink>
+              <NavLink
+                to='/dashboard/users/add/'
+                exact
+                strict
+                className="navbar-item"
+                activeClassName="is-active"
+              >               
+                Adicionar
+              </NavLink>
+              
+            </div>
+          </div>
+          <div className="navbar-item has-dropdown is-hoverable">
+            <NavLink
+              to='/dashboard/products/'
+              exact
+              strict
+              className="navbar-link"
+              activeClassName="is-active"
+            >
+              Products
+            </NavLink>
+            <div className="navbar-dropdown">
+              <NavLink
+                to='/dashboard/products/'
+                exact
+                strict
+                className="navbar-item"
+                activeClassName="is-active"
+              >               
+                Listar
+              </NavLink>
+              <NavLink
+                to='/dashboard/products/add/'
+                exact
+                strict
+                className="navbar-item"
+                activeClassName="is-active"
+              >               
+                Adicionar
+              </NavLink>
+              
+            </div>
+          </div>
+        </div>
+
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <div className="field is-grouped">
+              <p className="control">
+                <a className="button is-primary" href="https://github.com/jgthms/bulma/archive/0.5.1.zip">
+                  <span className="icon">
+                    <i className="fa fa-sign-out"></i>
+                  </span>
+                  <span>Logout</span>
+                </a>
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </nav>
