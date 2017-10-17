a2ca484bc43d5b777478c7ffd873311638f964ba
Lucas Simon <lucassrod@gmail.com>
2017-10-17 18:30:56 -0200

    Adiciona o redux na APP principal

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-
-import App from './App';
-
-import registerServiceWorker from './registerServiceWorker';
-
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import { store, history } from './store'
+import { ConnectedRouter } from 'react-router-redux'
 
 ReactDOM.render((
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store} >
+      <ConnectedRouter history={history}>
+        <App />
+      </ConnectedRouter>
+    </Provider>
     ), document.getElementById('root')
 );
 registerServiceWorker();
