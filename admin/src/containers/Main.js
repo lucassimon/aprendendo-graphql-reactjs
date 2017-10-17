7807aee9fe821a20c27c0eeb82632da828d56e75
Lucas Simon <lucassrod@gmail.com>
2017-10-17 18:31:26 -0200

    Corrige os imports dos componentes

--- admin/src/containers/Main.js
+++ admin/src/containers/Main.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import { Switch, Route} from 'react-router-dom'
 import NavigationTop from '../components/NavigationTop'
-import Dashboard from './Dashboard'
-import Users from './Users'
-import Products from './Products'
+import Dashboard from './Dashboard/Dashboard'
+import Users from './Users/Users'
+import Products from './Products/Products'
 
 const Main = ({match}) => (
   <div>
     <NavigationTop />
     <Switch>
       <Route exact path={`${match.url}/`} component={Dashboard}/>
-      <Route path={`${match.url}/users/`} component={Users} />
+      <Route path={`${match.url}/sellers/`} component={Users} />
       <Route path={`${match.url}/products/`} component={Products}/>
     </Switch>
   </div>
