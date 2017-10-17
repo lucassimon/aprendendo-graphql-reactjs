1a63b79a2eaeaeaf7e3921509d6ab383b78c3313
Lucas Simon <lucassrod@gmail.com>
2017-10-17 18:32:30 -0200

    Cria a store principal do projeto

---
+++ admin/src/store.js
@@ -0,0 +1,44 @@
+import { createStore, applyMiddleware, combineReducers } from 'redux'
+import createSagaMiddleware from 'redux-saga';
+import { reducers as authReducers, sagas as authSagas } from './containers/Auth'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import createHistory from 'history/createBrowserHistory'
+import { routerReducer, routerMiddleware } from 'react-router-redux'
+
+// Create a history of your choosing (we're using a browser history in this case)
+const history = createHistory()
+// Build the middleware for intercepting and dispatching navigation actions
+const routeMiddleware = routerMiddleware(history)
+
+const rootReducer = combineReducers({
+  auth: authReducers.auth,
+  route: routerReducer
+})
+
+const sagaMiddleware = createSagaMiddleware()
+
+const myLogger = (store) => (next) => (action) => {
+  console.log('Logged action', action)
+  next(action)
+}
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(
+    applyMiddleware(
+      myLogger,
+      routeMiddleware,
+      sagaMiddleware
+    )
+  )
+)
+
+function* rootSaga() {
+  yield [
+    authSagas()
+  ]
+}
+
+sagaMiddleware.run(rootSaga)
+
+export { store, history }
