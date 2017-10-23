e229e74596a6b476e2127fa14831e4dde1bdd87e
Lucas Simon <lucassrod@gmail.com>
2017-10-22 23:27:23 -0200

    Adiciona o redux ao login form

--- admin/src/containers/Auth/LoginForm.js
+++ admin/src/containers/Auth/LoginForm.js
@@ -1,29 +1,66 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { setLoginEmail, setLoginPassword, submitLoginForm } from './actions'
+import classNames from 'classnames'
 
-const LoginForm = () => {
+const LoginForm = ({ loginForm, ...actions }) => {
+  
+  const btnLoginClass = classNames({
+    'button is-primary is-large': true,
+    'is-loading': loginForm.loading
+  });
+  
   return (
-    <form>
+    <form onSubmit={actions.onFormSubmit}>
+      { loginForm.error &&
+      <div className="notification is-danger">
+          {loginForm.errors}
+      </div>
+      }
       <div className="field">
         <div className="control">
-          <input className="input is-large" type="email" placeholder="Your Email" autofocus="" />
+          <input
+            className="input"
+            type="email"
+            name="email"
+            value={loginForm.email.value}
+            placeholder="Your Email"
+            required
+            onChange={actions.onEmailChange}
+          />
         </div>
       </div>
 
       <div className="field">
         <div className="control">
-          <input className="input is-large" type="password" placeholder="Your Password" />
+          <input
+            className="input"
+            type="password"
+            name="password"
+            value={loginForm.password.value}
+            placeholder="Your Password"
+            onChange={actions.onPasswordChange}
+          />
         </div>
       </div>
-      <div className="field">
-        <label className="checkbox">
-          <input type="checkbox" />
-          Remember me
-        </label>
-      </div>
-      <a className="button is-block is-primary is-alt">Login</a>
+      <button className={btnLoginClass} value="Login"> Login </button>
     </form>
   )
 }
 
 
-export default LoginForm
+const mapStateToProps = (state) => ({
+  loginForm: state.loginForm
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  onEmailChange(e) { dispatch(setLoginEmail(e.target.value)) },
+  onPasswordChange(e) { dispatch(setLoginPassword(e.target.value)) },
+  onFormSubmit(e) {
+    e.preventDefault()
+    dispatch(submitLoginForm())
+  }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(LoginForm);
+
