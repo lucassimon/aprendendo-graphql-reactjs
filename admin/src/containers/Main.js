56d7e7b4350f43574f8cb8cd07d55799a086549e
Lucas Simon <lucassrod@gmail.com>
2017-10-22 23:24:30 -0200

    Adiciona componentes privados

--- admin/src/containers/Main.js
+++ admin/src/containers/Main.js
@@ -1,19 +1,45 @@
 import React from 'react'
-import { Switch, Route} from 'react-router-dom'
+import { Switch, Route, Redirect} from 'react-router-dom'
 import NavigationTop from '../components/NavigationTop'
 import Dashboard from './Dashboard/Dashboard'
 import Users from './Users/Users'
 import Products from './Products/Products'
+import { connect } from 'react-redux'
+import Error404 from './404'
 
-const Main = ({match}) => (
+
+function PrivateRoute ({component: Component, authed, active, admin, ...rest}) {
+
+  const isValid = authed && active && admin
+  return (
+    <Route
+      {...rest}
+      render={(props) => isValid
+        ? <Component {...props} />
+        : <Redirect to={{pathname: '/login', state: {from: props.location}}} />}
+    />
+  )
+}
+  
+
+const Main = ({match, authed, active, admin}) => (
   <div>
     <NavigationTop />
     <Switch>
-      <Route exact path={`${match.url}/`} component={Dashboard}/>
-      <Route path={`${match.url}/sellers/`} component={Users} />
-      <Route path={`${match.url}/products/`} component={Products}/>
+      <PrivateRoute authed={authed} active={active} admin={admin} exact path={`${match.url}`} component={Dashboard}/>
+      <PrivateRoute authed={authed} active={active} admin={admin} path={`${match.url}/sellers`} component={Users} />
+      <PrivateRoute authed={authed} active={active} admin={admin} path={`${match.url}/products`} component={Products}/>
+      <Route component={Error404}/>
     </Switch>
   </div>
 )
 
-export default Main
+
+const mapStateToProps = (state) => ({
+  authed: state.auth.authenticated,
+  active: state.profile.user.active,
+  admin: state.profile.permissions.admin,
+})
+
+
+export default connect(mapStateToProps)(Main);
