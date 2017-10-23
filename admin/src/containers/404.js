8e489a30a0a6e717f529080203f12dc96c803439
Lucas Simon <lucassrod@gmail.com>
2017-10-22 23:29:04 -0200

    Cria rotas de erro

---
+++ admin/src/containers/404.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const Error404 = () => {
+
+  return (
+    <h2> Acesse os links abaixo para acessar o dashboard </h2>
+  )
+}
+export default Error404
---
+++ admin/src/containers/NoMatch.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const NoMatch = () => {
+
+  return (
+    <h2> Nao encontrado </h2>
+  )
+}
+export default NoMatch
