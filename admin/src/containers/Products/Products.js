ebf2d2d7546d020e795cc5d15fa5b1a369dc567b
Lucas Simon <lucassrod@gmail.com>
2017-10-22 23:24:01 -0200

    Remove a ultima / da url

--- admin/src/containers/Products/Products.js
+++ admin/src/containers/Products/Products.js
@@ -7,9 +7,9 @@ import ProductDetail from './ProductDetail'
 const Courses = ({match}) => {
   return (
     <Switch>
-      <Route exact path={`${match.url}/`} component={ProductList} />
-      <Route path={`${match.url}/add/`} component={ProductAdd}/>
-      <Route path={`${match.url}/detail/`} component={ProductDetail}/>
+      <Route exact path={`${match.url}`} component={ProductList} />
+      <Route path={`${match.url}/add`} component={ProductAdd}/>
+      <Route path={`${match.url}/detail`} component={ProductDetail}/>
     </Switch>
   )
 }
