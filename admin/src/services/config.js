35b5ddb06a5a617f34a4749bc9f8a61429edbf9e
Lucas Simon <lucassrod@gmail.com>
2017-10-22 23:22:09 -0200

    Altera o base url para localhost:5000

--- admin/src/services/config.js
+++ admin/src/services/config.js
@@ -1,6 +1,6 @@
 const config = {
   'development': {
-    'base': 'http://localhost:4000'
+    'base': 'http://localhost:5000'
   },
   'production': {
     'base': 'https://graphqlcourse.herokuapp.com'
