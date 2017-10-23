234cfc7a560ee0627643c14593a6d9fd185e4c1f
Lucas Simon <lucassrod@gmail.com>
2017-10-22 23:26:29 -0200

    Cria as actions para autenticaco

--- admin/src/containers/Auth/actions.js
+++ admin/src/containers/Auth/actions.js
@@ -1,19 +1,46 @@
-export const SET_USER = 'Auth/SET_USER'
-export const UNSET_USER = 'Auth/UNSET_USER'
-export const SET_NAME = 'Auth/SET_NAME'
-export const SET_EMAIL = 'Auth/SET_EMAIL'
-export const SET_PASSWORD = 'Auth/SET_PASSWORD'
-export const SET_PASSWORD_CONFIRM = 'Auth/SET_PASSWORD_CONFIRM'
-export const SUBMIT_SIGNUP_FORM = 'Auth/SUBMIT_SIGNUP_FORM'
-export const SUBMIT_LOGIN_FORM = 'Auth/SUBMIT_LOGIN_FORM'
-export const CLEAR_AUTH_FORM = 'Auth/CLEAR_AUTH_FORM'
+export const SET_AUTH_TOKEN = 'Auth/SET_AUTH_TOKEN'
+export const setAuthToken = (jwts) => ({ type: SET_AUTH_TOKEN, payload: jwts })
+export const CLEAR_AUTH = 'Auth/CLEAR_AUTH'
+export const clearAuth = () => ({ type: CLEAR_AUTH })
 
-// SignUp & Login.
-export const setName = (name) => ({ type: SET_NAME, payload: name })
-export const setEmail = (email) => ({ type: SET_EMAIL, payload: email })
-export const setPassword = (password) => ({ type: SET_PASSWORD, payload: password })
-export const setPasswordConfirm = (password) => ({ type: SET_PASSWORD_CONFIRM, payload: password })
-export const submitSignUpForm = () => ({ type: SUBMIT_SIGNUP_FORM })
-export const submitLoginForm = () => ({ type: SUBMIT_LOGIN_FORM })
-export const clearAuthForm = () => ({ type: CLEAR_AUTH_FORM })
+// Login & Login Form
 
+export const LOGIN_START = 'Auth/LOGIN_START'
+export const loginStart = () => ({ type: LOGIN_START })
+export const LOGIN_SUCCESS = 'Auth/LOGIN_SUCCESS'
+export const loginSuccess = () => ({ type: LOGIN_SUCCESS })
+export const LOGIN_ERROR = 'Auth/LOGIN_ERROR'
+export const loginError = (error) => ({ type: LOGIN_ERROR, payload: error });
+export const LOGIN_SET_EMAIL = 'Auth/LOGIN_SET_EMAIL'
+export const setLoginEmail = (email) => ({ type: LOGIN_SET_EMAIL, payload: email });
+export const LOGIN_SET_PASSWORD = 'Auth/LOGIN_SET_PASSWORD'
+export const setLoginPassword = (password) => ({ type: LOGIN_SET_PASSWORD, payload: password });
+export const LOGIN_SUBMIT_FORM = 'Auth/LOGIN_SUBMIT_FORM'
+export const submitLoginForm = () => ({ type: LOGIN_SUBMIT_FORM })
+export const LOGIN_CLEAR_FORM = 'Auth/LOGIN_CLEAR_FORM'
+export const clearLoginForm = () => ({ type: LOGIN_CLEAR_FORM })
+
+// JWT
+export const SAVE_JWT_LOCAL_STORAGE = 'Auth/SAVE_JWT_LOCAL_STORAGE'
+export const saveJWTLocalStorage = () => ({ type: SAVE_JWT_LOCAL_STORAGE })
+
+// Profile
+export const LOGGEDIN_PROFILE_START = 'Auth/LOGGEDIN_PROFILE_START'
+export const loggedInStart = () => ({ type: LOGGEDIN_PROFILE_START })
+export const LOGGEDIN_PROFILE_ERROR = 'Auth/LOGGEDIN_PROFILE_ERROR'
+export const loggedInError = () => ({ type: LOGGEDIN_PROFILE_ERROR })
+export const LOGGEDIN_PROFILE_SUCCESS = 'Auth/LOGGEDIN_PROFILE_SUCCESS'
+export const loggedInSuccess = () => ({ type: LOGGEDIN_PROFILE_SUCCESS })
+
+export const SET_PROFILE_LOGGED_IN = 'Auth/SET_PROFILE_LOGGED_IN'
+export const setProfileLoggedIn = (profile) => ({ type: SET_PROFILE_LOGGED_IN, payload: profile })
+
+// Permissions
+
+// Logout
+export const LOGOUT_START = 'Auth/LOGOUT_START'
+export const logoutStart = () => ({ type: LOGOUT_START })
+export const LOGOUT_SUCCESS = 'Auth/LOGOUT_SUCCESS'
+export const logoutSuccess = () => ({ type: LOGOUT_SUCCESS })
+export const LOGOUT_ERROR = 'Auth/LOGOUT_ERROR'
+export const logoutError = (error) => ({ type: LOGOUT_ERROR, payload: error });
