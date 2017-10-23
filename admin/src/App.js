011ac21631eb6b31f8f1d385d091dbd7e56972e6
Lucas Simon <lucassrod@gmail.com>
2017-10-22 23:28:39 -0200

    Corrige as rotas da app

--- admin/src/App.js
+++ admin/src/App.js
@@ -7,16 +7,18 @@ import Main from './containers/Main'
 import Login from './containers/Auth/Login'
 import Signup from './containers/Auth/Signup'
 import OauthSuccess from './containers/OauthSuccess'
+import NoMatch from './containers/NoMatch'
 
 class App extends Component {
   render() {
     return (
       <Switch>
         <Route exact path="/" component={Index} /> {/* Note-1 */}
-        <Route exact path='/login/' component={Login} />
-        <Route path='/register/' component={Signup} />
-        <Route path='/dashboard/' component={Main} />
-        <Route path='/oauth/ok/' component={OauthSuccess} />
+        <Route path='/login' component={Login} />
+        <Route path='/register' component={Signup} />
+        <Route path='/dashboard' component={Main} />
+        <Route path='/oauth/ok' component={OauthSuccess} />
+        <Route component={NoMatch} />
       </Switch>
     );
   }
